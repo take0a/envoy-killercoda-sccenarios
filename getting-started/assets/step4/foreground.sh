b92d7c02fcb20e147bd6987c39364b5302641f0b
ARAKI Takeo <araki.takeo@robon.co.jp>
2024-08-31 07:48:23 +0000

    間違い修正

--- getting-started/assets/step4/foreground.sh
+++ getting-started/assets/step4/foreground.sh
@@ -1,4 +1,4 @@
-apt autoremove docker-compose -y
+apt-get autoremove docker-compose -y
 curl -SL https://github.com/docker/compose/releases/download/v2.29.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 mkdir -p ~/envoy/examples/
