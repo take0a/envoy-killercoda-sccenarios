3374ff12de4819c71ee1f7a74325b4d1ea7f8309
ARAKI Takeo <araki.takeo@robon.co.jp>
2024-08-31 07:23:09 +0000

    step4 の asset の追加とdocker-composeの更新

--- getting-started/assets/step4/foreground.sh
+++ getting-started/assets/step4/foreground.sh
@@ -1,2 +1,6 @@
+apt remove docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v2.29.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 mkdir -p ~/envoy/examples/
 cp -r ~/assets/step4/front-proxy/ ~/envoy/examples/
+cp -r ~/assets/step4/shared/ ~/envoy/examples/
