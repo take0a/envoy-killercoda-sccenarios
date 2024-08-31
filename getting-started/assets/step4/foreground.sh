b09ca7da56b0c2d534d3ceb85f555f76f67b3bb8
ARAKI Takeo <araki.takeo@robon.co.jp>
2024-08-31 08:00:45 +0000

    まだ駄目

--- getting-started/assets/step4/foreground.sh
+++ getting-started/assets/step4/foreground.sh
@@ -1,6 +1,8 @@
+set +e
 apt-get autoremove docker-compose -y
 curl -SL https://github.com/docker/compose/releases/download/v2.29.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 mkdir -p ~/envoy/examples/
 cp -r ~/assets/step4/front-proxy/ ~/envoy/examples/
 cp -r ~/assets/step4/shared/ ~/envoy/examples/
